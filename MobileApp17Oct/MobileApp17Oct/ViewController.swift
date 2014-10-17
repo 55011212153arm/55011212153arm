6e83ec2e73e63c4ae948037c1e297411e73128b5
55011212021prince <iMac@MSU>
2014-10-17 15:26:48 +0700

    fsdfsdf

---
+++ MobileApp17Oct/MobileApp17Oct/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileApp17Oct
+//
+//  Created by Student on 10/17/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MobileApp17Oct/MobileApp17Oct/colortwoCcntroller.swift
@@ -0,0 +1,13 @@
+//
+//  colortwoCcntroller.swift
+//  MobileApp17Oct
+//
+//  Created by Student on 10/17/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+import UIKit
+
+class colortwoCcntroller: NSObject {
+   
+}
